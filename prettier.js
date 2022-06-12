b8f99037e156e347adcbfb5cb4ff312f2e476aba
Fedir97 <106695191+Fedir97@users.noreply.github.com>
2022-06-12 15:29:03 +0300

    Add files via upload

---
+++ prettier.js
@@ -0,0 +1,119 @@
+< !DOCTYPE html >
+    <html lang="ru">
+
+        <head>
+            <link rel="stylesheet" href="./css/style.css" />
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
+                <link href="https://fonts.googleapis.com/css2?family=Roboto:ital@1&display=swap" rel="stylesheet">
+                    <meta charset="UTF-8">
+                        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                <title>Document</title>
+                            </head>
+
+                            <body>
+                                <header>
+                                    <nav>
+                                        <a href="">WebStudio
+                                        </a>
+                                        <ul class="social-links">
+                                            <li><a class="link" href="">Студия</a></li>
+                                            <li><a class="link" href="">Портфолио</a></li>
+                                            <li><a class="link" href="">Портфолио</a></li>
+                                            <li><a class="link" href="">Контакты</a></li>
+                                        </ul>
+                                    </nav>
+                                    <ul class="social-links4">
+                                        <li><a class="link" href="mailto:info@devstudio.com">info@devstudio.com</a>
+                                        </li>
+                                        <li><a class="link" href="tel:+380961111111">+380961111111</a>
+                                        </li>
+                                    </ul>
+
+                                </header>
+
+                                <main>
+                                    <h1>
+
+                                        <section>
+                                            <ul class="social-links2">
+                                                <li>
+                                                    <button type="button">Все</button>
+                                                </li>
+                                                <li> <button type="button">Веб-сайты</button>
+                                                </li>
+                                                <li> <button type="button">Приложения</button>
+                                                </li>
+                                                <li> <button type="button">Дизайн</button>
+                                                </li>
+                                                <li> <button type="button">Маркетинг</button>
+                                                </li>
+
+                                            </ul>
+                                            <ul class="social-links3">
+                                                <li> <img src="./images/first.jpg" alt="" width="370" />
+                                                    <h3>Технокряк</h3>
+                                                    <p>Веб-сайт</p>
+                                                </li>
+
+                                                <li> <img src="./images/second.jpg" alt="" width="370" />
+                                                    <h3>Постер New Orlean vs Golden Star</h3>
+                                                    <p>Дизайн</p>
+                                                </li>
+                                                <li> <img src="./images/third.jpg" alt="" width="370" />
+                                                    <h3>Ресторан Seafood</h3>
+                                                    <p>Приложение</p>
+                                                </li>
+                                                <li> <img src="./images/fourth.jpg" alt="" width="370" />
+                                                    <h3>Проект Prime</h3>
+                                                    <p>Маркетинг</p>
+                                                </li>
+                                                <li> <img src="./images/fivth.jpg" alt="" width="370" />
+                                                    <h3>Проект Boxes</h3>
+                                                    <p>Приложение</p>
+                                                </li>
+                                                <li> <img src="./images/sixth.jpg" alt="" width="370" />
+                                                    <h3> Inspiration has no Borders</h3>
+                                                    <p>Веб-сайт</p>
+                                                </li>
+                                                <li> <img src="./images/seven.jpg" alt="" width="370" />
+                                                    <h3>Издание Limited Edition</h3>
+                                                    <p>Дизайн</p>
+                                                </li>
+                                                <li> <img src="./images/eight.jpg" alt="" width="370" />
+                                                    <h3>Проект LAB</h3>
+                                                    <p>Маркетинг</p>
+                                                </li>
+                                                <li> <img src="./images/nine.jpg" alt="" width="370" />
+                                                    <h3>Growing Business</h3>
+                                                    <p>Приложение</p>
+                                                </li>
+                                            </ul>
+                                        </section>
+                                </main>
+
+                                <footer>
+                                    <address>
+                                        <a href="">WebStudio</a>
+                                        <ul class="social-links4">
+
+                                            <li>
+                                                <a class="link" href="https://goo.gl/maps/5YBFwy5f6ECoQT7T9" target="_blank"
+                                                    rel="noopener noreferrer">
+                                                    г.Киев,пр-т Леси Украинки,26
+                                                </a>
+
+                                            </li>
+                                            <li>
+                                                <a class="link" href="mailto:info@example.com">info@example.com</a>
+                                            </li>
+                                            <li>
+                                                <a class="link" href="tel:+380991111111">+380991111111</a>
+                                            </li>
+                                        </ul>
+                                    </address>
+                                </footer>
+
+                            </body>
+
+                        </html>
